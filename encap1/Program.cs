d0ded05c76eefe4b2c83b66bef047e8cea28bc45
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:23:30 +0800

    Add project files.

---
+++ encap1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace encap1
+{
+
+    class myApp1
+    {
+
+        public static int a;
+        public static int b;
+        public static int c;
+      
+      
+        static void Data()
+        {
+
+            a = 10;
+            b = 20;
+            c=a+b;
+           
+        
+
+            Console.WriteLine("a=", a);
+            Console.WriteLine("b=", b);
+            Console.WriteLine("total=", c);
+
+        }
+        public void getdata()
+        {
+           
+            Data();
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            myApp1 add = new myApp1();
+           
+            add.getdata();
+
+        }
+    }
+}
